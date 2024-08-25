2921b1ed3dea7e42b8d46a5604d6b5bd74a48002
michalak111 <michalak111@gmail.com>
2024-08-25 14:49:15 +0200

    cleanup CookingTemperature styling

--- components/CookingPanel/CookingTemperature.tsx
+++ components/CookingPanel/CookingTemperature.tsx
@@ -1,47 +1,59 @@
 import { View } from "@/components/View";
 import { Text } from "@/components/Text";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
 
 type Props = { current: string; target: string };
 
 export const CookingTemperature = ({ current, target }: Props) => {
   return (
-    <View
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        padding: 0,
-      }}
-    >
+    <View style={styles.container}>
       <View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <Text style={{ fontSize: 80, fontWeight: "bold", lineHeight: 80 }}>{current}</Text>
-          <MaterialCommunityIcons name="temperature-celsius" size={24} color="black" />
+        <View style={styles.tempCol}>
+          <Text style={styles.textCurrent}>{current}</Text>
+          <CelciusIcon />
         </View>
-        <Text style={{ fontSize: 14, lineHeight: 14, textAlign: "center", marginRight: 20 }}>
-          Current{"\n"}temperature
-        </Text>
+        <Text style={[styles.textDescription, { marginRight: 20 }]}>Current{"\n"}temperature</Text>
       </View>
+
       <View style={{ height: "50%", marginTop: 20 }}>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <Text style={{ fontSize: 40, fontWeight: "bold", lineHeight: 40 }}>/ {target}</Text>
-          <MaterialCommunityIcons name="temperature-celsius" size={24} color="black" />
+        <View style={styles.tempCol}>
+          <Text style={styles.textTarget}>/ {target}</Text>
+          <CelciusIcon />
         </View>
-        <Text style={{ fontSize: 14, lineHeight: 14, textAlign: "center", marginTop: 20 }}>
-          Target{"\n"}temperature
-        </Text>
+        <Text style={[styles.textDescription, { marginTop: 20 }]}>Target{"\n"}temperature</Text>
       </View>
     </View>
   );
 };
+
+const CelciusIcon = () => {
+  return <MaterialCommunityIcons name="temperature-celsius" size={24} color="black" />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  tempCol: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  textCurrent: {
+    fontSize: 80,
+    fontWeight: "bold",
+    lineHeight: 80,
+  },
+  textTarget: {
+    fontSize: 40,
+    fontWeight: "bold",
+    lineHeight: 40,
+  },
+  textDescription: {
+    fontSize: 14,
+    lineHeight: 14,
+    textAlign: "center",
+  },
+});
